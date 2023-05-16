d060b3df3b587eda697e93222ecb9cc3784c9fe3
Serj112 <raskolnikov.112@mail.ru>
2023-05-16 15:34:34 +0300

    Final

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -4,3 +4,26 @@
         `<p>${review['comment']}</p>` +
         '</div>';
 }
+
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -51,32 +51,6 @@ setTimeout(() =>
     alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
     30000);
 
-
-
-function getReview() {
-    // Создадим объект
-    let review = {}
-
-    // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if (review["userName"] == null) {
-        return
-    }
-
-    // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if (review["comment"] == null) {
-        return
-    }
-
-    // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
-
-    // Добавим на страницу
-    writeReview(review)
-}
-
-
 /*function handleSession() {
    
     // Сохраним UserAgentт 
